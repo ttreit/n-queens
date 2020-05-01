52712a8e27bb34aa3817acd69a1815c4e6b9e019
ttreit <14933277+ttreit@users.noreply.github.com>
2020-05-01 16:23:17 -0700

    Return to check all original values
    Had restricted checks to n greater than 3 to avoid
    edge cases in development of main solution.

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -38,7 +38,7 @@ describe('solvers', function() {
     it('finds a valid solution for n of 0-7', function() {
       // Skip 2 and 3 because they have no solution.
       //TODO replace 0 and 1 in following array
-      [4, 5, 6, 7, 8].map(function(n) {
+      [0, 1, 4, 5, 6, 7, 8].map(function(n) {
         var solutionBoard = new Board(findNQueensSolution(n));
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
           return memo + _.reduce(row, function(memo, col) {
